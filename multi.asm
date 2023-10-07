5d018dddc43c2f8af5b5e202b96a788ae5d54728
a8dogdark <a8dogdark@gmail.com>
2023-10-07 18:09:13 -0300

    07-10-2023

--- multi.asm
+++ multi.asm
@@ -5,12 +5,25 @@
     icl 'base/loader.asm'
     icl 'base/pagina7.asm'
     icl 'base/pagina4.asm'
+dls
+:3    .by $70
+    .by $46
+    .wo show
+    .by $41
+    .wo dls
+show
+    .sb " "
+    .sb +128,"dogdark"
+    .sb "  softwares "
 dos
     jmp ($0c)
 @start
     jsr dos
 start
-
+    ldx #<dls
+    stx $230
+    ldy #>dls
+    sty $231
     lda #$02
     sta 710
     sta 712
@@ -18,3 +31,4 @@ start
 inicio
 
     jmp start
+    run inicio
